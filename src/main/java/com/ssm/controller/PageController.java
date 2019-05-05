a93b057296159a5217836ebb16792571a5538e48
zyx <1194537927@qq.com>
2019-05-05 14:43:41 +0800

    springmvc搭建

---
+++ src/main/java/com/ssm/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/index")
+    public ModelAndView Main(){
+        return new ModelAndView("index");
+    }
+}
